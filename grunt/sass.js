ee1fb8094ad84ccb52fa3fd8278cd6877d6105f5
James Grubb <james@jamesgrubb.co.uk>
2015-06-26 00:05:56 +0100

    resolving mrmrs colors
    Signed-off-by: James Grubb <james@jamesgrubb.co.uk>

---
+++ grunt/sass.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dist: {
+      options: {
+        style: 'expanded',
+        require: 'susy'
+      },
+      files: {
+        'css/style.css': 'scss/style.scss'
+      }
+    }
+}
